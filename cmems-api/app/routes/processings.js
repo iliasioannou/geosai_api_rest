f4e31be7db6e8d84ef53f90e5edd5c1076abf723
Maurizio Cosmai <cosmai@planetek.it>
2017-07-04 17:21:27 +0200

    processing request

---
+++ cmems-api/app/routes/processings.js
@@ -0,0 +1,131 @@
+"use strict";
+const routes = function(server) {
+    const confRoutes = require("./config");
+    const namespace = require("restify-namespace");
+    const serviceUsers = require("../services/users");
+    const serviceProcessings = require("../services/processings");
+    const restify = require("restify");
+    const logger = require("../utils/log");
+    const chalk = require("chalk");
+
+    const logMetadata = {
+        module: "routes.processing",
+        method: ""
+    };
+
+
+    const PATH = "/processings";
+    namespace(server, PATH, function() {
+
+        /**
+         * @api {post} /processings Add Processing
+         * @apiVersion 1.0.0
+         * @apiName AddProcessing
+         * @apiGroup Processing
+         * @apiDescription Add processing
+         *
+         * @apiExample {curl} Example usage:
+         *    curl -H "Content-Type: application/json" -X POST -d '{"email": "nome.cognome@ispra.it","name": "nome","surname": "cognome","username": "username","password": "pass","idRole": 3,"idAmp": null,"idCnr": null,"idArpa": 1,"idOrganizationType": 4,"active": "1"}' http://localhost:3000/api/v1/users
+         *
+         * @apiSuccessExample {json} Success-Response:
+         * 	HTTP/1.1  200 OK
+         * 	Content-Type: application/json
+         *  {"email": "nome.cognome@ispra.it","name": "nome","surname": "cognome","username": "username","password": "pass","idRole": 3,"idArpa": 1,"idOrganizationType": 4,"active": 1,"idUser": 6}
+         *
+         * @apiErrorExample  {json} Error-Response:
+         * 	HTTP/1.1  409 Conflict
+         * 	Content-Type: application/json
+         *  {"code":"ConflictError","message":"Already exist user with same username"}
+         *
+         * @apiErrorExample  {json} Error-Response:
+         * 	HTTP/1.1  501 InternalServerError
+         * 	Content-Type: application/json
+         *
+         */
+        server.post({
+            path: "/",
+            version: confRoutes.VERSION_1_0_0
+        }, addProcessing);
+
+    });
+
+    function addProcessing(req, res, next) {
+        logMetadata.method = "AddProcessing";
+        logger.log.debug(logger.utils.dividerDots, logMetadata);
+
+        logger.log.debug("Richiesta: " + req);
+        logger.log.debug("username in Richiesta: <" + req.username + ">");
+
+        if (!req.body || req.body === "") {
+            next(new restify.BadRequestError("Invalid body"));
+        }
+        if (!req.is('json') && !req.is('application/json')) {
+            next(new restify.BadRequestError("Invalid body. Must be a JSON"));
+        }
+
+        let inputJson = req.body;
+        logger.log.debug("Body: %s", JSON.stringify(inputJson), logMetadata);
+
+        let username = req.username;
+
+        serviceUsers.existUser(username)
+            .then(function(exist) {
+                logger.log.debug("Username -%s- exist ? %s", username, (exist ? "Y" : "N"), logMetadata);
+                if (!exist) {
+                    next(new restify.InternalServerError("User does not exist"));
+                }
+                else {
+                    serviceUsers.getUserByUsername(username)
+                      .then(function(user){
+                          let userObject = user.toJSON();
+                          let email = userObject.email;
+                          logger.log.debug("User data: " + JSON.stringify(user) );
+
+                          if(userObject.username === 'anonymous'){
+                              next(new restify.InvalidCredentialsError("Unauthorized user."));
+                          }
+
+                          let processingParams = {
+                            userEmail: email,
+                            processingInputData: inputJson
+                          };
+
+                          serviceProcessings.addProcessing(processingParams)
+                              .then(function(done){
+                                if(done){
+                                  res.send(done);
+                                }
+                              })
+                              .catch(function(error){
+                                  next(new restify.InternalServerError(error));
+                              })
+
+                      })
+                      .catch(function(error){
+                          logger.log.error(error, logMetadata);
+                          next(new restify.InternalServerError("Problem with retrieving the user."));
+                      })
+
+                    serviceUsers.addUser(userParams)
+                        .then(function(user) {
+                            logger.log.debug("user created : %s", JSON.stringify(user.toJSON()), logMetadata);
+                            res.send(user.toJSON());
+                        })
+                        .catch(function(error) {
+                            logger.log.error(error, logMetadata);
+                            next(new restify.InternalServerError("Problem during add user."));
+                        });
+                }
+            });
+
+
+
+
+
+    }
+
+}
+
+
+
+module.exports = routes;
---
+++ cmems-api/app/services/processings.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let logger = require("../utils/log");
+let chalk = require('chalk');
+let request = require('request');
+
+let logMetadata = {
+    module: "services.processing",
+    method: ""
+};
+
+
+
+/**
+ * Add processing
+ */
+let addProcessing = function(processingParams) {
+    logMetadata.method = "addProcessing";
+    logger.log.debug(logger.utils.dividerDots, logMetadata);
+    logger.log.debug("input: %s", JSON.stringify(processingParams), logMetadata);
+
+    return new Promise(function(resolve, reject) {
+
+      var options = {
+        uri: 'http://temisto.planetek.it:9999/processings',
+        method: 'POST',
+        json: processingParams
+      };
+      request(options, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+          resolve(true);
+        } else {
+          reject(error);
+        }
+      });
+    });
+}
+
+// Public
+let pub = {
+    addProcessing: addProcessing
+}
+
+module.exports = pub
